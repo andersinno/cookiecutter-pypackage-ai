18d858f30cbd6addf571df832e886b1ddbd7495e
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-03-29 15:51:21 +0300

    Fix title underlining length in README.rst

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,4 @@
+import io
 import os
 import shutil
 
@@ -10,3 +11,15 @@ if is_it_django_package != 'yes':
     # Remove the tests dir as its Django specific
     tests_dir = os.path.join(python_pkg_dir, 'tests')
     shutil.rmtree(tests_dir)
+
+
+# Fix title line length in README.rst
+with io.open('README.rst', 'rt', encoding='utf-8') as fp:
+    lines = iter(fp)
+    first_line = next(lines)
+    second_line = next(lines)
+    assert set(second_line.strip()) == set(['='])
+    underlining = (len(first_line.rstrip()) * '=') + '\n'
+    new_lines = [first_line, underlining] + list(lines)
+with io.open('README.rst', 'wt', encoding='utf-8') as fp:
+    fp.write(''.join(new_lines))
